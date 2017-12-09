9e314597138f5e5b5860ff6adf124cbc6b6d66e2
YonePC <YonePC@YONE>
2017-12-09 14:43:44 +0000

    annotations

--- test/TrackingServiceTest.java
+++ test/TrackingServiceTest.java
@@ -1,6 +1,4 @@
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
@@ -8,9 +6,25 @@ public class TrackingServiceTest {
 
     private TrackingService trackingService;
 
+    @BeforeClass
+    public static void init(){
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void exit(){
+        System.out.println("After Class");
+    }
+
     @Before
     public void setUp() throws Exception {
+        System.out.println("Before");
         trackingService = new TrackingService();
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("After");
     }
 
     @Test
